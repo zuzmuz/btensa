6bf302e9de53e3794bc7f2a93e76d56ab5d38b5d
zuzmuz <hamadehzaher0@gmail.com>
2024-03-20 22:55:44 +0200

    removing test code

--- models.go
+++ models.go
@@ -1,17 +1,16 @@
 package main
 
-
 type Secret struct {
-    Title string `json:"title"`
-    Value string `json:"value"`
-    Ishidden bool `json:"isHidder"`
+	Title    string `json:"title"`
+	Value    string `json:"value"`
+	Ishidden bool   `json:"isHidder"`
 }
 
 type Capsule struct {
-    Name string `json:"name"`
-    Secrets []Secret `json:"secrets"`
+	Name    string   `json:"name"`
+	Secrets []Secret `json:"secrets"`
 }
 
 type Vault struct {
-    Capsules []Capsule `json:"capsule"`
+	Capsules []Capsule `json:"capsule"`
 }
